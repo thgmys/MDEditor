29796b425dc93f89170f686eb70d7cfb2a5a9b68
thgmys <malysagh@tcd.ie>
2024-02-17 17:46:37 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { marked } from 'marked'
+import { debounce } from 'lodash-es'
+import { ref, computed } from 'vue'
+
+const input = ref('# Welcome to MDEditor')
+
+const output = computed(() => marked(input.value))
+
+const update = debounce((e) => {
+  input.value = e.target.value
+}, 100)
+</script>
+
+<template>
+  <div class="topnav">
+    <a class="active" href="#home">name of file</a>
+    <a href="#about">New File</a>
+    <a href="#news">Save File</a>
+    <a href="#contact">Open File</a>
+  </div>
+  <div class="editor">
+    <textarea class="input" :value="input" @input="update"></textarea>
+    <div class="output" v-html="output"></div>
+  </div>
+</template>
