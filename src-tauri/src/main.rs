a4f6df8e0b97562a7ef1a232759c433115659c2f
thgmys <malysagh@tcd.ie>
2024-02-19 21:43:59 +0000

    a

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -13,3 +13,166 @@ fn main() {
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
+// use std::thread;
+
+// const NTHREADS: u32 = 10;
+
+// // This is the `main` thread
+// fn main() {
+//     // Make a vector to hold the children which are spawned.
+//     let mut children = vec![];
+
+//     for i in 0..NTHREADS {
+//         // Spin up another thread
+//         children.push(thread::spawn(move || {
+//             println!("this is thread number {}", i);
+//         }));
+//     }
+
+//     for child in children {
+//         // Wait for the thread to finish. Returns a result.
+//         let _ = child.join();
+//     }
+// }
+
+
+// use std::fs::File;
+// use std::io::prelude::*;
+// use std::path::Path;
+
+// fn main() {
+//     // Create a path to the desired file
+//     let path = Path::new("hello.txt");
+//     let display = path.display();
+
+//     // Open the path in read-only mode, returns `io::Result<File>`
+//     let mut file = match File::open(&path) {
+//         Err(why) => panic!("couldn't open {}: {}", display, why),
+//         Ok(file) => file,
+//     };
+
+//     // Read the file contents into a string, returns `io::Result<usize>`
+//     let mut s = String::new();
+//     match file.read_to_string(&mut s) {
+//         Err(why) => panic!("couldn't read {}: {}", display, why),
+//         Ok(_) => print!("{} contains:\n{}", display, s),
+//     }
+
+//     // `file` goes out of scope, and the "hello.txt" file gets closed
+// }
+
+// static LOREM_IPSUM: &str =
+//     "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+// tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+// quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+// consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+// cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+// proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+// ";
+
+// use std::fs::File;
+// use std::io::prelude::*;
+// use std::path::Path;
+
+// fn main() {
+//     let path = Path::new("lorem_ipsum.txt");
+//     let display = path.display();
+
+//     // Open a file in write-only mode, returns `io::Result<File>`
+//     let mut file = match File::create(&path) {
+//         Err(why) => panic!("couldn't create {}: {}", display, why),
+//         Ok(file) => file,
+//     };
+
+//     // Write the `LOREM_IPSUM` string to `file`, returns `io::Result<()>`
+//     match file.write_all(LOREM_IPSUM.as_bytes()) {
+//         Err(why) => panic!("couldn't write to {}: {}", display, why),
+//         Ok(_) => println!("successfully wrote to {}", display),
+//     }
+// }
+
+// use std::fs::read_to_string;
+
+// fn read_lines(filename: &str) -> Vec<String> {
+//     let mut result = Vec::new();
+
+//     for line in read_to_string(filename).unwrap().lines() {
+//         result.push(line.to_string())
+//     }
+
+//     result
+// }
+
+
+// use std::convert::TryFrom;
+// use std::convert::TryInto;
+
+// #[derive(Debug, PartialEq)]
+// struct EvenNumber(i32);
+
+// impl TryFrom<i32> for EvenNumber {
+//     type Error = ();
+
+//     fn try_from(value: i32) -> Result<Self, Self::Error> {
+//         if value % 2 == 0 {
+//             Ok(EvenNumber(value))
+//         } else {
+//             Err(())
+//         }
+//     }
+// }
+
+// fn main() {
+//     // TryFrom
+
+//     assert_eq!(EvenNumber::try_from(8), Ok(EvenNumber(8)));
+//     assert_eq!(EvenNumber::try_from(5), Err(()));
+
+//     // TryInto
+
+//     let result: Result<EvenNumber, ()> = 8i32.try_into();
+//     assert_eq!(result, Ok(EvenNumber(8)));
+//     let result: Result<EvenNumber, ()> = 5i32.try_into();
+//     assert_eq!(result, Err(()));
+// }
+
+
+
+// // Unlike C/C++, there's no restriction on the order of function definitions
+// fn main() {
+//     // We can use this function here, and define it somewhere later
+//     fizzbuzz_to(100);
+// }
+
+// // Function that returns a boolean value
+// fn is_divisible_by(lhs: u32, rhs: u32) -> bool {
+//     // Corner case, early return
+//     if rhs == 0 {
+//         return false;
+//     }
+
+//     // This is an expression, the `return` keyword is not necessary here
+//     lhs % rhs == 0
+// }
+
+// // Functions that "don't" return a value, actually return the unit type `()`
+// fn fizzbuzz(n: u32) -> () {
+//     if is_divisible_by(n, 15) {
+//         println!("fizzbuzz");
+//     } else if is_divisible_by(n, 3) {
+//         println!("fizz");
+//     } else if is_divisible_by(n, 5) {
+//         println!("buzz");
+//     } else {
+//         println!("{}", n);
+//     }
+// }
+
+// // When a function returns `()`, the return type can be omitted from the
+// // signature
+// fn fizzbuzz_to(n: u32) {
+//     for n in 1..=n {
+//         fizzbuzz(n);
+//     }
+// }
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script setup>
-import { marked } from 'marked'
-import { debounce } from 'lodash-es'
-import { ref, computed } from 'vue'
+import { marked } from "marked";
+import { debounce } from "lodash-es";
+import { ref, computed } from "vue";
 
-const input = ref('# Welcome to MDEditor')
+const input = ref("# Welcome to MDEditor");
 
-const output = computed(() => marked(input.value))
+const output = computed(() => marked(input.value));
 
 const update = debounce((e) => {
-  input.value = e.target.value
-}, 100)
+  input.value = e.target.value;
+}, 100);
 </script>
 
 <template>
@@ -24,3 +24,323 @@ const update = debounce((e) => {
     <div class="output" v-html="output"></div>
   </div>
 </template>
+
+<!--
+
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('Hello World!')
+
+function reverseMessage() {
+  // Access/mutate the value of a ref via
+  // its .value property.
+  message.value = message.value.split('').reverse().join('')
+}
+
+function notify() {
+  alert('navigation was prevented.')
+}
+</script>
+
+<template>
+
+  <h1>{{ message }}</h1>
+
+  
+  <button @click="reverseMessage">Reverse Message</button>
+
+  <button @click="message += '!'">Append "!"</button>
+
+
+  <a href="https://vuejs.org" @click.prevent="notify">
+    A link with e.preventDefault()
+  </a>
+</template>
+
+<style>
+button, a {
+  display: block;
+  margin-bottom: 1em;
+}
+</style> 
+
+
+
+<script setup>
+import { ref, watchEffect } from 'vue'
+
+const API_URL = `https://api.github.com/repos/vuejs/core/commits?per_page=3&sha=`
+const branches = ['main', 'v2-compat']
+
+const currentBranch = ref(branches[0])
+const commits = ref(null)
+
+watchEffect(async () => {
+  // this effect will run immediately and then
+  // re-run whenever currentBranch.value changes
+  const url = `${API_URL}${currentBranch.value}`
+  commits.value = await (await fetch(url)).json()
+})
+
+function truncate(v) {
+  const newline = v.indexOf('\n')
+  return newline > 0 ? v.slice(0, newline) : v
+}
+
+function formatDate(v) {
+  return v.replace(/T|Z/g, ' ')
+}
+</script>
+
+<template>
+  <h1>Latest Vue Core Commits</h1>
+  <template v-for="branch in branches">
+    <input type="radio"
+      :id="branch"
+      :value="branch"
+      name="branch"
+      v-model="currentBranch">
+    <label :for="branch">{{ branch }}</label>
+  </template>
+  <p>vuejs/vue@{{ currentBranch }}</p>
+  <ul>
+    <li v-for="{ html_url, sha, author, commit } in commits">
+      <a :href="html_url" target="_blank" class="commit">{{ sha.slice(0, 7) }}</a>
+      - <span class="message">{{ truncate(commit.message) }}</span><br>
+      by <span class="author">
+        <a :href="author.html_url" target="_blank">{{ commit.author.name }}</a>
+      </span>
+      at <span class="date">{{ formatDate(commit.author.date) }}</span>
+    </li>
+  </ul>
+</template>
+
+<style>
+a {
+  text-decoration: none;
+  color: #42b883;
+}
+li {
+  line-height: 1.5em;
+  margin-bottom: 20px;
+}
+.author,
+.date {
+  font-weight: bold;
+}
+</style>
+
+
+
+<script setup>
+import { ref } from 'vue'
+import TreeItem from './TreeItem.vue'
+
+const treeData = ref({
+  name: 'My Tree',
+  children: [
+    { name: 'hello' },
+    { name: 'world' },
+    {
+      name: 'child folder',
+      children: [
+        {
+          name: 'child folder',
+          children: [{ name: 'hello' }, { name: 'world' }]
+        },
+        { name: 'hello' },
+        { name: 'world' },
+        {
+          name: 'child folder',
+          children: [{ name: 'hello' }, { name: 'world' }]
+        }
+      ]
+    }
+  ]
+})
+</script>
+
+<template>
+  <ul>
+    <TreeItem class="item" :model="treeData"></TreeItem>
+  </ul>
+</template>
+
+<style>
+.item {
+  cursor: pointer;
+  line-height: 1.5;
+}
+.bold {
+  font-weight: bold;
+}
+</style>
+
+
+
+
+
+
+
+
+<script setup>
+import { ref, shallowReactive, toRaw } from 'vue'
+
+const history = shallowReactive([[]])
+const index = ref(0)
+const circles = ref([])
+const selected = ref()
+const adjusting = ref(false)
+
+function onClick({ clientX: x, clientY: y }) {
+  if (adjusting.value) {
+    adjusting.value = false
+    selected.value = null
+    push()
+    return
+  }
+
+  selected.value = [...circles.value].reverse().find(({ cx, cy, r }) => {
+    const dx = cx - x
+    const dy = cy - y
+    return Math.sqrt(dx * dx + dy * dy) <= r
+  })
+
+  if (!selected.value) {
+    circles.value.push({
+      cx: x,
+      cy: y,
+      r: 50
+    })
+    push()
+  }
+}
+
+function adjust(circle) {
+  selected.value = circle
+  adjusting.value = true
+}
+
+function push() {
+  history.length = ++index.value
+  history.push(clone(circles.value))
+  console.log(toRaw(history))
+}
+
+function undo() {
+  circles.value = clone(history[--index.value])
+}
+
+function redo() {
+  circles.value = clone(history[++index.value])
+}
+
+function clone(circles) {
+  return circles.map((c) => ({ ...c }))
+}
+</script>
+
+<template>
+  <svg @click="onClick">
+    <foreignObject x="0" y="40%" width="100%" height="200">
+      <p class="tip">
+        Click on the canvas to draw a circle. Click on a circle to select it.
+        Right-click on the canvas to adjust the radius of the selected circle.
+      </p>
+    </foreignObject>
+    <circle
+      v-for="circle in circles"
+      :cx="circle.cx"
+      :cy="circle.cy"
+      :r="circle.r"
+      :fill="circle === selected ? '#ccc' : '#fff'"
+      @click="selected = circle"
+      @contextmenu.prevent="adjust(circle)"
+    ></circle>
+  </svg>
+
+  <div class="controls">
+    <button @click="undo" :disabled="index <= 0">Undo</button>
+    <button @click="redo" :disabled="index >= history.length - 1">Redo</button>
+  </div>
+
+  <div class="dialog" v-if="adjusting" @click.stop>
+    <p>Adjust radius of circle at ({{ selected.cx }}, {{ selected.cy }})</p>
+    <input type="range" v-model="selected.r" min="1" max="300">
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+  overflow: hidden;
+}
+
+svg {
+  width: 100vw;
+  height: 100vh;
+  background-color: #eee;
+}
+
+circle {
+  stroke: #000;
+}
+
+.controls {
+  position: fixed;
+  top: 10px;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+
+.controls button + button {
+  margin-left: 6px;
+}
+
+.dialog {
+  position: fixed;
+  top: calc(50% - 50px);
+  left: calc(50% - 175px);
+  background: #fff;
+  width: 350px;
+  height: 100px;
+  padding: 5px 20px;
+  box-sizing: border-box;
+  border-radius: 4px;
+  text-align: center;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
+}
+
+.dialog input {
+  display: block;
+  width: 200px;
+  margin: 0px auto;
+}
+
+.tip {
+  text-align: center;
+  padding: 0 50px;
+  color: #bbb;
+}
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-->
